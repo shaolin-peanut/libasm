9b768769ce2603ada51fae23bac24f32085bf915
shaolin-peanut <shaolinpeanut@gmail.com>
2024-08-21 11:40:04 +0200

    ft_strcmp seems to work

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,11 +2,30 @@ section .text
     global ft_strcmp
 
 ft_strcmp:
-    ; save pointers
-    mov r10, rdi
-    mov r11, rsi
-loop_start:
-    ; 
+    ; function start
+loop_start: ; load chars from s1 and s2
+    mov al , [rdi]
+    mov bl , [rsi]
+    jmp while
+
+while_body: ; increment pointers and jmp to start
+    inc rdi
+    inc rsi
+    jmp loop_start
+
+while: ; test for null termination and diff in loaded chars
+    test al, al
+    jz loop_end
+    test bl, bl
+    jz loop_end
+    cmp al, bl ; compare characters, set to 0 if equal, or 1 if not equal
+    jne loop_end ; jump if condition is met => jump to loop_start
+    jmp while_body
 
 loop_end:
+    ; s1 = 0, s2 = 'a' => return -1
+    ; s1 = 'a', s2 = 0 => return 1
+    sub al, bl
+    mov rax, rbx
+    ret
     
--- srcs/main.c
+++ srcs/main.c
@@ -2,6 +2,29 @@
 
 extern int ft_strlen(char *s);
 extern char *ft_strcpy(char *dest, char *src);
+extern int ft_strcmp(char *s1, char *s2);
+
+//  test ft_strlen
+// test ft_strcpy
+// test ft_strcmp
+// test ft_write
+// test ft_read
+// test ft_strdup
+void test_ft_strcmp(void) {
+    char s1[] = "hello";
+    char s2[] = "hello";
+    int result = ft_strcmp(s1, s2);
+    printf("Return value should be 0: %d\n", result);
+    s2[0] = 'j';
+    result = ft_strcmp(s1, s2);
+    printf("Return value should be 1: %d\n", result);
+    s1[0] = 'j';
+    s2[0] = 'h';
+    result = ft_strcmp(s1, s2);
+    printf("Return value should be -1: %d\n", result);
+    return;
+}
+
 
 int main() {
     int result = ft_strlen("hello");
@@ -9,5 +32,6 @@ int main() {
     char destination[100];
     char *strcpy_result = ft_strcpy(destination, "world");
     printf("destination: %s, strcpy_result: %s\n", destination, strcpy_result);
+    test_ft_strcmp();
     return 0;
 }
